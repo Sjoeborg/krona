4baf8b45f3f770f163fb7aa8f3b4c07ad8c280c3
Martin Sjöborg <martin@sjoborg.org>
2025-07-27 22:37:27 +0200

    symbol-specific action buffer so that we don't mix up splits

--- krona/processor/action.py
+++ krona/processor/action.py
@@ -1,6 +1,7 @@
 from krona.models.action import Action, ActionType
 from krona.models.position import Position
 from krona.models.transaction import Transaction
+from krona.processor.mapper import Mapper
 from krona.utils.logger import logger
 
 
@@ -8,20 +9,31 @@ class ActionProcessor:
     """Handles business logic for corporate actions"""
 
     def __init__(self) -> None:
-        self.transaction_buffer: list[Transaction] = []
+        self.transaction_buffer: dict[str, list[Transaction]] = {}
 
-    def handle_split(self, transaction: Transaction, position: Position) -> None:
+    def handle_split(self, transaction: Transaction, position: Position, mapper: Mapper) -> None:
         """Handle stock split by calculating the split ratio from two SPLIT transactions.
 
         If the two SPLIT transactions have the same date, we assume that the split is NOT a reverse split.
         TODO: implement user resolution for reverse splits.
         """
 
-        if len(self.transaction_buffer) == 0:
-            self.transaction_buffer.append(transaction)
+        if (
+            transaction.symbol not in self.transaction_buffer
+            and mapper.get_synonyms(transaction.symbol) not in self.transaction_buffer
+        ):
+            self.transaction_buffer[transaction.symbol] = [transaction]
             return
 
-        previous_transaction = self.transaction_buffer.pop()
+        try:
+            previous_transaction = self.transaction_buffer[transaction.symbol][0]
+            self.transaction_buffer.pop(transaction.symbol)
+        except KeyError:
+            syn = mapper.get_synonyms(transaction.symbol)
+            if syn is None:
+                raise
+            previous_transaction = self.transaction_buffer[syn][0]
+            self.transaction_buffer.pop(syn)
 
         # If dates are the same, ensure split ratio is greater than 1 by swapping transactions
         if previous_transaction.date == transaction.date and transaction.quantity / previous_transaction.quantity < 1:
@@ -41,8 +53,7 @@ class ActionProcessor:
             type=ActionType.SPLIT,
             ratio=split_ratio,
         )
-
-        logger.debug(
+        logger.info(
             f"Split {position.symbol} from {position.quantity} @ {position.price:.2f} to {int(position.quantity * split.ratio)} @ {(position.price / split.ratio):.2f} (split ratio: {split.ratio})"
         )
         # TODO: handle fractional shares
